df21ee744958c0bc909bb371561c82454f62c6eb
OMGLAM <cristianowirtz@gmail.com>
2025-04-11 16:46:21 +0100

    Create web.php

---
+++ web.php
@@ -0,0 +1,9 @@
+use App\Http\Controllers\PostController;
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
